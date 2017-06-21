f4ad525ae1efa463a25451ff4cc6d6fe15782274
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-06-21 23:04:27 +0300

    Cpu: Add new

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 pub struct Cpu {
     a:  u16,  // Accumulator
     x:  u16,  // Index register
@@ -9,4 +10,21 @@ pub struct Cpu {
     pb: u8,   // Data bank
     db: u8,   // Program cunter bank
     e:  bool, // Emulation mode
+}
+
+impl Cpu {
+    pub fn new() -> Cpu {
+        Cpu {
+            a:  0x00,
+            x:  0x00,
+            y:  0x00,
+            pc: 0x00,
+            s:  0xFF,
+            p:  0b0011_0100,
+            d : 0x00,
+            pb: 0x0,
+            db: 0x0,
+            e:  true,
+        }
+    }
 }
