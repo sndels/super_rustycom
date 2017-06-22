a74f93adcf670d393fac7b946eb83ffb949116ce
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-06-22 23:02:05 +0300

    Main: Run cpu

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ use std::env;
 
 mod abus;
 use abus::ABus;
+mod cpu;
+use cpu::Cpu;
 
 fn main() {
     let rom_path = env::args().nth(1).expect("No rom defined");
     let mut abus = ABus::new(&rom_path);
+    let mut cpu = Cpu::new(&mut abus);
+    cpu.run(&mut abus);
 }
