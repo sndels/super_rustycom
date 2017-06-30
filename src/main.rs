10bc5cc653c564c1eb5118d9528da6a953e551b6
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-06-30 16:51:10 +0300

    Main: Use debugger

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,22 @@ mod abus;
 use abus::ABus;
 mod cpu;
 use cpu::Cpu;
+mod debugger;
+use debugger::Debugger;
 
 fn main() {
     let rom_path = env::args().nth(1).expect("No rom defined");
     let mut abus = ABus::new(&rom_path);
     let mut cpu = Cpu::new(&mut abus);
+    let mut debugger = Debugger::new();
     loop {
-        cpu.step(&mut abus);
+        if debugger.active {
+            debugger.take_command(&mut cpu, &mut abus);
+        } else {
+            while cpu.get_pb_pc() != debugger.breakpoint {
+                cpu.step(&mut abus);
+            }
+            debugger.active = true;
+        }
     }
 }
