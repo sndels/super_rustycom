530a8d191e24b570184f5a2e41a717e50c179edf
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2018-01-21 12:12:38 +0200

    Move loading ROM to client

--- super-rustycom-client/src/main.rs
+++ super-rustycom-client/src/main.rs
@@ -3,6 +3,8 @@ extern crate super_rustycom_core;
 mod debugger;
 
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 use super_rustycom_core::abus::ABus;
 use super_rustycom_core::cpu::W65C816S;
@@ -11,13 +13,27 @@ use debugger::Debugger;
 use debugger::disassemble_current;
 
 fn main() {
+    // Get ROM path from first argument
     let rom_path = env::args().nth(1).expect("No rom defined");
-    let mut abus = ABus::new(&rom_path);
+
+    // Load ROM from file
+    let mut rom_file = File::open(&rom_path).expect("Opening rom failed");
+    let mut rom_bytes = Vec::new();
+    let read_bytes = rom_file
+        .read_to_end(&mut rom_bytes)
+        .expect("Reading rom to bytes failed");
+    println!("Read {} bytes from {}", read_bytes, rom_path);
+
+    // Init hardware
+    let mut abus = ABus::new(rom_bytes);
     let mut cpu = W65C816S::new(&mut abus);
     let mut debugger = Debugger::new();
+
     // Hack past the apu check in elix_nu
     mmap::apu_write8(&mut abus, 0x00, 0xAA);
     mmap::apu_write8(&mut abus, 0x01, 0xBB);
+
+    // Run
     loop {
         if debugger.quit {
             break;
