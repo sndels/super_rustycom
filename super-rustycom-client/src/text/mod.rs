11f3b23d7ca3a01b7bece99896dfa5f503ad3dc2
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2020-10-27 22:02:04 +0200

    TextRenderer: Skip overflowing chars on line

--- super-rustycom-client/src/text/mod.rs
+++ super-rustycom-client/src/text/mod.rs
@@ -17,43 +17,39 @@ impl TextRenderer {
         }
     }
 
+    /// Draws the text in the given pixel buffer line by line.
+    /// Overflowing characters in either dimension are ignored.
     pub fn draw(&self, text: String, mut pixel_buffer: Vec<&mut [u32]>) {
         let window_width = pixel_buffer[0].len();
         let window_height = pixel_buffer.len();
-        assert!(window_width > self.font.width + self.char_spacing);
-        assert!(window_height > self.font.height + self.line_spacing);
+        if window_width == 0
+            || window_height == 0
+            || window_width <= (self.font.width + self.char_spacing)
+            || window_height <= (self.font.height + self.line_spacing)
+        {
+            return;
+        }
 
         let mut start_pixel_row = 0;
         let mut start_pixel_column = 0;
-        // Let's use a peekable iterator instead of straight loop since we need this in column logic
-        let mut iter = text.chars().into_iter().peekable();
-        while let Some(c) = iter.next() {
-            macro_rules! change_line {
-                () => {
-                    start_pixel_column = 0;
-                    start_pixel_row += self.font.height + self.line_spacing;
-                };
-            };
-
+        for c in text.chars() {
             // Make sure we don't run out of vertical pixels
             if start_pixel_row + self.font.height >= window_height {
                 break;
             }
 
             if c == '\n' {
-                change_line!();
+                start_pixel_column = 0;
+                start_pixel_row += self.font.height + self.line_spacing;
             } else {
-                self.draw_char(
-                    c,
-                    start_pixel_column,
-                    &mut pixel_buffer[start_pixel_row..start_pixel_row + self.font.height],
-                );
-
-                start_pixel_column += self.font.width + self.char_spacing;
-                if iter.peek() != Some(&'\n')
-                    && start_pixel_column + self.font.width >= window_width
-                {
-                    change_line!();
+                // Don't draw if we've ran out of space on the line
+                if start_pixel_column + self.font.width < window_width {
+                    self.draw_char(
+                        c,
+                        start_pixel_column,
+                        &mut pixel_buffer[start_pixel_row..start_pixel_row + self.font.height],
+                    );
+                    start_pixel_column += self.font.width + self.char_spacing;
                 }
             }
         }
