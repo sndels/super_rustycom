f48309c732e6efd22049d48bfbf25de0ed96ffaa
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2020-10-31 18:19:01 +0200

    Framebuffer: Fix window limits, gather

--- super-rustycom-client/src/framebuffer.rs
+++ super-rustycom-client/src/framebuffer.rs
@@ -45,22 +45,28 @@ impl Framebuffer {
         let clamped_width = if left + width < self.width {
             width
         } else {
-            width - (left + width - self.width) - 1
+            width - (left + width - self.width)
         };
         let clamped_height = if top + height < self.height {
             height
         } else {
-            height - (top + height - self.height) - 1
+            height - (top + height - self.height)
         };
 
         let mut slices = Vec::new();
         let (_, mut rest) = self.buffer.split_at_mut(top * self.width + left);
-        for _ in 0..clamped_height {
+        // Do n-1 loops as priming after the last split will fail if the window
+        // extends to the last line of pixels
+        for _ in 0..clamped_height.saturating_sub(1) {
             let (head, tail) = rest.split_at_mut(clamped_width);
             slices.push(head);
+            // Split at next pixel in window to prime the next iteration
             let (_, tail) = tail.split_at_mut(self.width - clamped_width);
             rest = tail;
         }
+        let (head, _) = rest.split_at_mut(clamped_width);
+        slices.push(head);
+
         slices
     }
 }
